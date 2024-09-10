6ddcbca76b8ba3cf15cdd61a428f27970fcc3908
swachha prodip saha <88526062+swachhasaha@users.noreply.github.com>
2024-09-10 12:15:14 +0530

    Update stop_container.sh
    disable for now

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Stop the running container (if any)
-containerid = `docker ps |awk -F " "  '{print $1}' `
-if[$containerid==null];then
-  echo "okk"
-else
-  docker stop $containerid
-fi
+#containerid = `docker ps |awk -F " "  '{print $1}' `
+#if[$containerid==null];then
+#  echo "okk"
+#else
+ # docker stop $containerid
+#fi
